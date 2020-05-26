cbf76dacc0d7dfa7767af2cb00a64e243db9b093
bisjon <18rd115@ms.dendai.ac.jp>
2020-05-26 14:53:01 +0900

    Initial Commit

---
+++ tetris_projectTests/tetris_projectTests.swift
@@ -0,0 +1,34 @@
+//
+//  tetris_projectTests.swift
+//  tetris_projectTests
+//
+//  Created by 田中勇気 on 2020/05/26.
+//  Copyright © 2020 IOSapplication_time. All rights reserved.
+//
+
+import XCTest
+@testable import tetris_project
+
+class tetris_projectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
