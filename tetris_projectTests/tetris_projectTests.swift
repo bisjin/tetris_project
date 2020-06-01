b07351fbf33c296220d3ec7787fd10149a4e34c2
bisjin <18rd115@ms.dendai.ac.jp>
2020-06-01 16:31:52 +0900

    commitのテスト2

--- tetris_projectTests/tetris_projectTests.swift
+++ tetris_projectTests/tetris_projectTests.swift
@@ -14,6 +14,7 @@ class tetris_projectTests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
+    
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
