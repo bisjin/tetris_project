f9406adefc7806db7f8a039acb1dc4fdb9643005
bisjin <18rd115@ms.dendai.ac.jp>
2020-06-11 04:32:52 +0900

    ブロックの位置調整と配列を作っただけ、複数生成はまだ

--- tetris_project/GameViewController.swift
+++ tetris_project/GameViewController.swift
@@ -22,7 +22,12 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
+    //ブロックを定義
     var brock : UIView!
+    //終了変数
+    var finish:Bool = false
+    //test用の変数
+    var counter:Int = 0
     
     //struct brock_struct {
     //    let flg = 0
@@ -35,15 +40,25 @@ class GameViewController: UIViewController {
     
    //var timer:Timer = Timer()
     
-    //a[3][3]={0 1 0 0
-    //         0 1 0 0
-    //         0 1 0 0
-    //         0 1 0 0}
+    /*a[3][3]={0 1 0 0
+             0 1 0 0
+             0 1 0 0
+             0 1 0 0}*/
+    var bar = [
+    [0,1,0,0],
+    [0,1,0,0],
+    [0,1,0,0],
+    [0,1,0,0]]
     
-    //a2[3][3]={0 0 0 0
-    //          0 0 0 0
-    //          1 1 1 1
-    //          0 0 0 0}
+    /*a2[3][3]={0 0 0 0
+              0 0 0 0
+              1 1 1 1
+              0 0 0 0}*/
+    var yoko_bar = [
+    [0,0,0,0],
+    [0,0,0,0],
+    [1,1,1,1],
+    [0,0,0,0]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,15 +68,31 @@ class GameViewController: UIViewController {
             //screenHeight = view.frame.size.height
        
         //ブロックを生成
-        //if(brock.frame.origin.y == 180){
-            brock_create()
-        //}
+        //生成の基本関数
+        brock_create()
+        /*repeat{
+            switch counter {
+            case 0:
+                print("counter:\(counter)")
+                brock_create()
+                counter+=1
+            default :
+                //if(brock.frame.origin.y <= 240){
+                    print("aaaa")
+                       // brock = nil
+                        brock_create()
+                        counter+=1
+                //}
+            }
+        }while(counter==10)
+        //}while(!finish)*/
 //時間
  Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(GameViewController.gravity), userInfo: nil, repeats: true)
- }
+    }
+    
 //ブロック生成
     @objc func brock_create(){
-        brock = UIView(frame: CGRect(x:180, y:50, width:30, height:30))
+        brock = UIView(frame: CGRect(x:160, y:50, width:30, height:30))
         let bgColor = UIColor.blue
         brock.backgroundColor = bgColor
         // 枠線の色
@@ -72,9 +103,16 @@ class GameViewController: UIViewController {
         self.view.addSubview(brock)
     }
     
+    /*ブロックを配列の形式で表示
+    @objc func brock_array(){
+        for(){
+    
+        }
+    }*/
+    
 //重力
  @objc func gravity() {
-    if(brock.frame.origin.y < 180){
+    if(brock.frame.origin.y < 250){
     brock.frame.origin.y += 10
     }
  }
@@ -83,22 +121,22 @@ class GameViewController: UIViewController {
     //左ボタン
     @IBAction func left(_ sender: Any) {
         if(brock.frame.origin.x > 40
-            && brock.frame.origin.y < 180){
-        brock.frame.origin.x -= 40
+            && brock.frame.origin.y < 250){
+        brock.frame.origin.x -= 30
         }
         print("afterRect: \(brock.frame)")
     }
     //右ボタン
     @IBAction func right(_ sender: Any) {
-        if(brock.frame.origin.x < 320 && brock.frame.origin.y < 180){
-        brock.frame.origin.x += 40
+        if(brock.frame.origin.x < 310 && brock.frame.origin.y < 250){
+        brock.frame.origin.x += 30
         }
         print("afterRect: \(brock.frame)")
     }
     //下ボタン
     @IBAction func under(_ sender: Any) {
-        if(brock.frame.origin.y < 180){
-        brock.frame.origin.y = 180
+        if(brock.frame.origin.y < 250){
+        brock.frame.origin.y = 250
         }
         print("afterRect: \(brock.frame)")
     }
@@ -107,5 +145,5 @@ class GameViewController: UIViewController {
     
     //@IBAction func rightturn(_ sender: Any) {}
     
-
+    
 }
